f32d40df821491461db6dfec914d86cd64945114
Jordan Irwin <antumdeluge@gmail.com>
2021-05-31 10:36:20 -0700

    Split chatlog into files separated by date by default

--- init.lua
+++ init.lua
@@ -19,12 +19,28 @@ end
 
 
 chatlog.format = core.settings:get("chatlog.format") or "%m/%d/%y %X"
+chatlog.single_file = core.settings:get_bool("chatlog.single_file", false)
 
-chatlog.out = minetest.get_worldpath() .. "/chatlog.txt"
+chatlog.out = minetest.get_worldpath() .. "/chatlog"
+if chatlog.single_file then
+	chatlog.out = chatlog.out .. ".txt"
+end
 
 
 local function playerspeak(name, msg)
-	local f = io.open(chatlog.out, "a")
+	local f
+	if chatlog.single_file then
+		f = io.open(chatlog.out, "a")
+	else
+		-- make sure directory exists
+		if not core.mkdir(chatlog.out) then
+			chatlog.log("error", "could not create directory for writing: " .. chatlog.out)
+			return
+		end
+
+		f = io.open(chatlog.out .. "/" .. os.date("%Y_%m_%d") .. ".txt", "a")
+	end
+
 	if f then
 		f:write(os.date("(" .. chatlog.format .. ") [" .. name .. "]: " .. msg .. "\n"))
 		f:close()
