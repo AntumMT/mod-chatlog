6a1659086e324ec9d4e6eeaa386c1e2199408b71
Jordan Irwin <antumdeluge@gmail.com>
2021-05-31 10:16:30 -0700

    Make main function local

--- init.lua
+++ init.lua
@@ -4,7 +4,8 @@ chatlog = minetest.get_worldpath() .. "/chatlog.txt"
 
 chatlog.format = core.settings:get("chatlog.format") or "%m/%d/%y %X"
 
-function playerspeak(name, msg)
+
+local function playerspeak(name, msg)
 	local f = io.open(chatlog, "a")
 	f:write(os.date("(" .. chatlog.format .. ") [" .. name .. "]: " .. msg .. "\n"))
 	f:close()
