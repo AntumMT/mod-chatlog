2f5435865101245c274918ed982d0c4a2a5d73ca
Jordan Irwin <antumdeluge@gmail.com>
2021-05-31 10:26:54 -0700

    Print error if chatlog file cannot be opened for writing

--- init.lua
+++ init.lua
@@ -1,15 +1,35 @@
 -- License: MIT
 
-chatlog = minetest.get_worldpath() .. "/chatlog.txt"
+chatlog = {}
+chatlog.modname = core.get_current_modname()
+
+function chatlog.log(lvl, msg)
+	if not msg then
+		msg = lvl
+		lvl = nil
+	end
+
+	msg = "[" .. chatlog.modname .. "] " .. msg
+	if not lvl then
+		core.log(msg)
+	else
+		core.log(lvl, msg)
+	end
+end
+
 
 chatlog.format = core.settings:get("chatlog.format") or "%m/%d/%y %X"
 
+chatlog.out = minetest.get_worldpath() .. "/chatlog.txt"
+
 
 local function playerspeak(name, msg)
-	local f = io.open(chatlog, "a")
+	local f = io.open(chatlog.out, "a")
 	if f then
 		f:write(os.date("(" .. chatlog.format .. ") [" .. name .. "]: " .. msg .. "\n"))
 		f:close()
+	else
+		chatlog.log("error", "could not open chatlog file for writing: " .. chatlog.out)
 	end
 end
 
